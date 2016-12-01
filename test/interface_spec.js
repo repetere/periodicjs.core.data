895b81a72a7bfcf9cec0a6b2af0f07dbafb01063
janbialostok <janbialostok@gmail.com>
2016-12-01 16:51:14 -0500

    init

--- test/interface_spec.js
+++ test/interface_spec.js
@@ -1,1 +1,19 @@
 'use strict';
+
+require('mocha');
+const path = require('path');
+const Promisie = require('promisie');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('DB Adapter Interface', function () {
+	describe('basic assumptions and methods', function () {
+		it('Should create an ')
+	});
+	describe('generating a pre-loaded adapter by adapter name', function () {
+
+	});
+	describe('generating an adapter from a provided constructor', function () {
+
+	});
+});
